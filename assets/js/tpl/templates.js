a9decdb4a515100ebffa447b84d4dcb0215156a5
Nicolas MEDDA <nicolas.medda@ling.fr>
2014-02-04 17:57:32 +0100

    Fix layout + ajout d'icone

--- assets/js/tpl/templates.js
+++ assets/js/tpl/templates.js
@@ -12,7 +12,7 @@ var out='<div class="editor-wrapper"><div class="toolbar"></div><div class="cont
 var out='<div class="preview hidden"></div>';return out;
 };
   tmpl['editor-toolbar']=function anonymous(it) {
-var out='<div class="title"><input type="text" name="title" value="'+( it.title )+'" /></div><div class="pull-right"><a id="save" href="/page/'+( it.id !== undefined ? it.id + '/' : '' )+'save">Enregistrer</a><button id="show-preview">preview</button></div><div><input type="file" id="upload"/></div>';return out;
+var out='<div class="title"><input type="text" name="title" value="'+( it.title )+'" /></div><div class="pull-right"><a class="icon icon-save" id="save" href="/page/'+( it.id !== undefined ? it.id + '/' : '' )+'save">Enregistrer</a><a class="icon icon-preview" id="show-preview">preview</a></div><div><input type="file" id="upload"/></div>';return out;
 };
   tmpl['list-main']=function anonymous(it) {
 var out='<h3> Pages </h3><a href="/page/add" class="">Ajouter une page</a><ul class="pages-list">';if(it.length > 0){var arr1=it;if(arr1){var page,index=-1,l1=arr1.length-1;while(index<l1){page=arr1[index+=1];out+='<li class="page" data-page-id="'+( page.id )+'"><a href="/page/'+( page.id )+'">'+( page.title )+'</a></li>';} } }else{out+='<p class="empty-page-list">Aucune page pour l\'instant. Pour créer votre première page, utiliser le lien ci-dessus.</p>';}out+='</ul>';return out;
