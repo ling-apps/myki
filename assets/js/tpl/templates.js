54bc058fd68fd2aeb9dd40c21c03e3b517c659cb
Nicolas MEDDA <nicolas.medda@ling.fr>
2014-02-05 16:39:02 +0100

    titre de la page en cours (show/edit) + edit-in-place en mode edition

--- assets/js/tpl/templates.js
+++ assets/js/tpl/templates.js
@@ -12,7 +12,7 @@ var out='<div class="toolbar"></div><div class="content"></div><div class="previ
 var out='';return out;
 };
   tmpl['editor-toolbar']=function anonymous(it) {
-var out='<div class="title"><input type="text" name="title" value="'+( it.title )+'" /></div><div class="pull-right"><a class="icon icon-save" id="save" href="/page/'+( it.id !== undefined ? it.id + '/' : '' )+'save">Enregistrer</a><a class="icon icon-preview" id="show-preview">preview</a></div><div><input type="file" id="upload"/></div>';return out;
+var out='<div class="title"><div class="show">'+( it.title )+'</div><input class="edit" type="text" name="title" value="'+( it.title )+'" /></div><div class="pull-right"><a class="icon icon-save" id="save" href="/page/'+( it.id !== undefined ? it.id + '/' : '' )+'save">Enregistrer</a><a class="icon icon-preview" id="show-preview">preview</a></div><div><input type="file" id="upload"/></div>';return out;
 };
   tmpl['list-main']=function anonymous(it) {
 var out='<h3> Pages </h3><a href="/page/add" class="">Ajouter une page</a><ul class="pages-list">';if(it.length > 0){var arr1=it;if(arr1){var page,index=-1,l1=arr1.length-1;while(index<l1){page=arr1[index+=1];out+='<li class="page" data-page-id="'+( page.id )+'"><a href="/page/'+( page.id )+'">'+( page.title )+'</a></li>';} } }else{out+='<p class="empty-page-list">Aucune page pour l\'instant. Pour créer votre première page, utiliser le lien ci-dessus.</p>';}out+='</ul>';return out;
@@ -24,6 +24,6 @@ var out='<div class="show-wrapper"><div class="toolbar"></div><div class="conten
 var out=''+( it );return out;
 };
   tmpl['show-toolbar']=function anonymous(it) {
-var out='<a class="edit" id="edit-page" href="/page/'+( it.id )+'/edit">Editer</a>';return out;
+var out='<a class="icon icon-edit" id="edit-page" href="/page/'+( it.id )+'/edit">Editer</a><div class="title">'+( it.title )+'</div>';return out;
 };
 return tmpl;})();
--- assets/js/views/editorView.js
+++ assets/js/views/editorView.js
@@ -23,28 +23,46 @@ EditorView.prototype.render = function(data) {
     this.$el.querySelector('.content').innerHTML = editor_t(data);
 
     this.$el.querySelector('.preview').innerHTML = marked(data.content,{renderer: customRenderer});
+    
+    // Titre - edit-in-place 
+    var titleWrapper = this.$el.querySelector('.title');
+    this.$el.querySelector('.title .show').addEventListener('click', function(e) {
+        titleWrapper.classList.add('editing');
+        var editInput = titleWrapper.querySelector('.edit');
+        editInput.value = e.target.textContent;
+        editInput.focus();
+    }, false);
 
+    this.$el.querySelector('.title .edit').addEventListener('blur', function(e) {
+        titleWrapper.classList.remove('editing');
+        titleWrapper.querySelector('.show').innerHTML = e.target.value;
+    });
 
+    // Render - essaie d'intégration d'image
     var customRenderer = new marked.Renderer();
     customRenderer.image = function(text, level) {
         var file = this.controller.getFile(text);
         return '<div>'+file+'TEST RENDER</div>';
     }.bind(this);
 
+    // Mise en place de Code Mirror
     this.cm = CodeMirror(this.$el.querySelector('#cm'), {
         value: data.content || '',
         mode: 'markdown'
     });
 
+    // Update de la preview à chaque modification de Code Mirror
     this.cm.on('change', function(e) {
         this.$el.querySelector('.preview').innerHTML = marked(this.getValue(),{renderer: customRenderer});
     }.bind(this));
 
+    // Afficher / Masquer la preview
     this.$el.querySelector('#show-preview').addEventListener('click', function() {
         this.$el.classList.toggle('with-preview');
         this.$el.querySelector('.preview').classList.toggle('hidden');
     }.bind(this), false);
 
+    // Sauvegarde
     this.$el.querySelector('#save').addEventListener('click', function(e) {
         e.preventDefault();
         var page = {
@@ -57,17 +75,18 @@ EditorView.prototype.render = function(data) {
         //p(url, {page: page});
     }.bind(this));
 
+    // Upload d'un fichier
     this.$el.querySelector('#upload').addEventListener('change',function(e){
     	var fr = new FileReader();
-	var file = e.currentTarget.files[0];
-	fr.readAsText(file, "ASCII");
+    	var file = e.currentTarget.files[0];
+    	fr.readAsText(file, "ASCII");
 
-	fr.onload = function(evt) {
- 		alert(evt.target.result);
-   		var div = document.createElement("div");
-		div.innerHTML = evt.target.result;
-		this.cm.addWidget({line: null, ch: null}, div,true);
-	}.bind(this);
+	    fr.onload = function(evt) {
+ 		    alert(evt.target.result);
+       		var div = document.createElement("div");
+    		div.innerHTML = evt.target.result;
+    		this.cm.addWidget({line: null, ch: null}, div,true);
+    	}.bind(this);
     }.bind(this));
 };
 
