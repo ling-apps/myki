896d48c7ee8b11dcc10303bebf21c62027627f9b
Nicolas MEDDA <nicolas.medda@ling.fr>
2014-03-04 17:02:21 +0100

    Petit server node pour fournir le client et que les urls fonctionne tout le temps

---
+++ bin/server.js
@@ -0,0 +1,40 @@
+var http = require('http');
+var fs = require('fs');
+var mime = require('mime');
+
+http.createServer(function(req, res) {
+    var template = 'mon super template';
+
+    // Static js file
+    if (/\.js$/.test(req.url)) {
+        var mimeType = mime.lookup(__dirname + '/../public/' + req.url);
+        var jsContent = fs.readFileSync(__dirname + '/../public/' + req.url);
+        res.writeHead(200, {'Content-Type': mimeType });
+        res.end(jsContent);
+    }
+
+    // Static images file
+    else if (/img\/.*$/.test(req.url)) {
+        var mimeType = mime.lookup(__dirname + '/../public/' + req.url);
+        var image = fs.readFileSync(__dirname + '/../public/' + req.url);
+        res.writeHead(200, {'Content-Type': mimeType });
+        res.end(image);
+    }
+
+    // Static css file
+    else if (/\.css$/.test(req.url)) {
+        var mimeType = mime.lookup(__dirname + '/../public/' + req.url);
+        var css = fs.readFileSync(__dirname + '/../public/' + req.url);
+        res.writeHead(200, {'Content-Type': mimeType });
+        res.end(css);
+    }
+
+    else {
+        // Serve home page
+        var mimeType = mime.lookup(__dirname + '/../public/index.html');
+        template = fs.readFileSync(__dirname + '/../public/index.html');
+        res.writeHead(200, {'Content-Type': mimeType });
+        res.end(template);
+    }
+
+}).listen(8080);
--- config.rb
+++ config.rb
@@ -1,6 +1,6 @@
 http_page = '/public'
 http_images_path = '/public'
-http_generated_images_path = '../img'
+http_generated_images_path = '/img'
 sprite_load_path = 'assets/img'
 generated_images_dir = 'public/img'
 additional_import_path = ['assets/img/']
