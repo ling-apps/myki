44f538bce6de8b12c69db0c3491f68c2c640ec17
Nicolas MEDDA <nicolas.medda@ling.fr>
2014-03-18 14:36:42 +0100

    Translate every text in english

--- assets/js/tpl/templates.js
+++ assets/js/tpl/templates.js
@@ -3,7 +3,7 @@ function encodeHTMLSource() {  var encodeHTMLRules = { "&": "&#38;", "<": "&#60;
 String.prototype.encodeHTML=encodeHTMLSource();
 var tmpl = {};
   tmpl['editor-insertImage']=function anonymous(it) {
-var out='<h2>Inserer une image</h2><form class="add-from-url"><input type="text" name="url" placeholder="Url de l\'image" /></form><ul class="images">';var arr1=it;if(arr1){var image,index=-1,l1=arr1.length-1;while(index<l1){image=arr1[index+=1];out+='<li class="image" data-image-title="'+( image.name )+'" data-image-src="data:image/png;base64,'+( image.content ||'').toString().encodeHTML()+'">'+( image.name )+'</li>';} } out+='</ul>';return out;
+var out='<h2>Insert an image</h2><form class="add-from-url"><input type="text" name="url" placeholder="Image URL" /></form><ul class="images">';var arr1=it;if(arr1){var image,index=-1,l1=arr1.length-1;while(index<l1){image=arr1[index+=1];out+='<li class="image" data-image-title="'+( image.name )+'" data-image-src="data:image/png;base64,'+( image.content ||'').toString().encodeHTML()+'">'+( image.name )+'</li>';} } out+='</ul>';return out;
 };
   tmpl['editor-main']=function anonymous(it) {
 var out='<div class="toolbar"></div><div class="content"></div><div class="preview hidden"></div><div class="images-list hidden"></div>';return out;
@@ -12,16 +12,16 @@ var out='<div class="toolbar"></div><div class="content"></div><div class="previ
 var out='';return out;
 };
   tmpl['editor-toolbar']=function anonymous(it) {
-var out='<div class="pull-left"><a alt="Enregistrer" class="icon icon-save" id="save" href="/pages/'+( it.id !== undefined ? it.id + '/' : '' )+'save">Enregistrer</a><a alt="afficher/masquer la prévisualisation" class="icon icon-preview" id="show-preview">preview</a><a alt="supprimer" class="icon icon-delete" id="delete">delete</a></div><div class="title"><div class="show">'+( it.title )+'</div><input class="edit" type="text" name="title" value="'+( it.title )+'" /></div><div class="pull-right"><a alt="Inserer une image" href="" class="icon icon-insert-images" id="insertimage">Inserer une image</a></div>';return out;
+var out='<div class="pull-left"><a alt="Save" class="icon icon-save" id="save" href="/pages/'+( it.id !== undefined ? it.id + '/' : '' )+'save">Save</a><a alt="Toggle preview" class="icon icon-preview" id="show-preview">Preview</a><a alt="Deltet" class="icon icon-delete" id="delete">Delete</a></div><div class="title"><div class="show">'+( it.title )+'</div><input class="edit" type="text" name="title" value="'+( it.title )+'" /></div><div class="pull-right"><a alt="Insert an image" href="" class="icon icon-insert-images" id="insertimage">Insert an image</a></div>';return out;
 };
   tmpl['files-list']=function anonymous(it) {
-var out='<h3> Files </h3><label for="upload-file">Ajouter</label><input type="file" id="upload-file" />';if(it.length > 0){out+='<ul class="menu files-list">';var arr1=it;if(arr1){var file,index=-1,l1=arr1.length-1;while(index<l1){file=arr1[index+=1];out+='<li class="file" data-file-id="'+( file.id )+'"><a href="/files/'+( file.id )+'">'+( file.name )+'</a></li>';} } out+='</ul>';}else{out+='<div class="menu files-list"><p class="empty-page-list">Aucune fichier pour l\'instant. Pour ajouter votre premier fichier, utiliser le lien ci-dessus.</p></div>';}return out;
+var out='<h3> Files </h3><label for="upload-file">Upload a new file</label><input type="file" id="upload-file" />';if(it.length > 0){out+='<ul class="menu files-list">';var arr1=it;if(arr1){var file,index=-1,l1=arr1.length-1;while(index<l1){file=arr1[index+=1];out+='<li class="item file" data-file-id="'+( file.id )+'"><a href="/files/'+( file.id )+'">'+( file.name )+'</a></li>';} } out+='</ul>';}else{out+='<div class="menu files-list"><p class="empty-page-list">You did not have upload any file now. Click on the link above to add your first one.</p></div>';}return out;
 };
   tmpl['files-show']=function anonymous(it) {
 var out='<img alt="'+( it.name ||'').toString().encodeHTML()+'" src="'+( it.content )+'" />';return out;
 };
   tmpl['list-main']=function anonymous(it) {
-var out='<h3> Pages </h3><a href="/pages/add" class="" id="add-page">Ajouter une page</a><ul class="menu pages-list">';if(it.length > 0){var arr1=it;if(arr1){var page,index=-1,l1=arr1.length-1;while(index<l1){page=arr1[index+=1];out+='<li class="item" data-page-id="'+( page.id )+'"><a href="/pages/'+( page.id )+'">'+( page.title )+'</a></li>';} } }else{out+='<p class="empty-page-list">Aucune page pour l\'instant. Pour créer votre première page, utiliser le lien ci-dessus.</p>';}out+='</ul>';return out;
+var out='<h3> Pages </h3><a href="/pages/add" class="" id="add-page">Add a page</a><ul class="menu pages-list">';if(it.length > 0){var arr1=it;if(arr1){var page,index=-1,l1=arr1.length-1;while(index<l1){page=arr1[index+=1];out+='<li class="item" data-page-id="'+( page.id )+'"><a href="/pages/'+( page.id )+'">'+( page.title )+'</a></li>';} } }else{out+='<p class="empty-page-list">There\'s no page for now. Click on the link above to create your first one.</p>';}out+='</ul>';return out;
 };
   tmpl['show-main']=function anonymous(it) {
 var out='<div class="show-wrapper"><div class="toolbar"></div><div class="content"></div></div>';return out;
@@ -30,6 +30,6 @@ var out='<div class="show-wrapper"><div class="toolbar"></div><div class="conten
 var out=''+( it );return out;
 };
   tmpl['show-toolbar']=function anonymous(it) {
-var out='<a class="icon icon-edit" id="edit-page" href="/pages/'+( it.id )+'/edit">Editer</a><div class="title">'+( it.title )+'</div>';return out;
+var out='<a alt="Edit" class="icon icon-edit" id="edit-page" href="/pages/'+( it.id )+'/edit">Edit</a><div class="title">'+( it.title )+'</div>';return out;
 };
 return tmpl;})();
--- public/js/app.js
+++ public/js/app.js
@@ -700,7 +700,7 @@ function encodeHTMLSource() {  var encodeHTMLRules = { "&": "&#38;", "<": "&#60;
 String.prototype.encodeHTML=encodeHTMLSource();
 var tmpl = {};
   tmpl['editor-insertImage']=function anonymous(it) {
-var out='<h2>Inserer une image</h2><form class="add-from-url"><input type="text" name="url" placeholder="Url de l\'image" /></form><ul class="images">';var arr1=it;if(arr1){var image,index=-1,l1=arr1.length-1;while(index<l1){image=arr1[index+=1];out+='<li class="image" data-image-title="'+( image.name )+'" data-image-src="data:image/png;base64,'+( image.content ||'').toString().encodeHTML()+'">'+( image.name )+'</li>';} } out+='</ul>';return out;
+var out='<h2>Insert an image</h2><form class="add-from-url"><input type="text" name="url" placeholder="Image URL" /></form><ul class="images">';var arr1=it;if(arr1){var image,index=-1,l1=arr1.length-1;while(index<l1){image=arr1[index+=1];out+='<li class="image" data-image-title="'+( image.name )+'" data-image-src="data:image/png;base64,'+( image.content ||'').toString().encodeHTML()+'">'+( image.name )+'</li>';} } out+='</ul>';return out;
 };
   tmpl['editor-main']=function anonymous(it) {
 var out='<div class="toolbar"></div><div class="content"></div><div class="preview hidden"></div><div class="images-list hidden"></div>';return out;
@@ -709,16 +709,16 @@ var out='<div class="toolbar"></div><div class="content"></div><div class="previ
 var out='';return out;
 };
   tmpl['editor-toolbar']=function anonymous(it) {
-var out='<div class="pull-left"><a alt="Enregistrer" class="icon icon-save" id="save" href="/pages/'+( it.id !== undefined ? it.id + '/' : '' )+'save">Enregistrer</a><a alt="afficher/masquer la prévisualisation" class="icon icon-preview" id="show-preview">preview</a><a alt="supprimer" class="icon icon-delete" id="delete">delete</a></div><div class="title"><div class="show">'+( it.title )+'</div><input class="edit" type="text" name="title" value="'+( it.title )+'" /></div><div class="pull-right"><a alt="Inserer une image" href="" class="icon icon-insert-images" id="insertimage">Inserer une image</a></div>';return out;
+var out='<div class="pull-left"><a alt="Save" class="icon icon-save" id="save" href="/pages/'+( it.id !== undefined ? it.id + '/' : '' )+'save">Save</a><a alt="Toggle preview" class="icon icon-preview" id="show-preview">Preview</a><a alt="Deltet" class="icon icon-delete" id="delete">Delete</a></div><div class="title"><div class="show">'+( it.title )+'</div><input class="edit" type="text" name="title" value="'+( it.title )+'" /></div><div class="pull-right"><a alt="Insert an image" href="" class="icon icon-insert-images" id="insertimage">Insert an image</a></div>';return out;
 };
   tmpl['files-list']=function anonymous(it) {
-var out='<h3> Files </h3><label for="upload-file">Ajouter</label><input type="file" id="upload-file" />';if(it.length > 0){out+='<ul class="menu files-list">';var arr1=it;if(arr1){var file,index=-1,l1=arr1.length-1;while(index<l1){file=arr1[index+=1];out+='<li class="file" data-file-id="'+( file.id )+'"><a href="/files/'+( file.id )+'">'+( file.name )+'</a></li>';} } out+='</ul>';}else{out+='<div class="menu files-list"><p class="empty-page-list">Aucune fichier pour l\'instant. Pour ajouter votre premier fichier, utiliser le lien ci-dessus.</p></div>';}return out;
+var out='<h3> Files </h3><label for="upload-file">Upload a new file</label><input type="file" id="upload-file" />';if(it.length > 0){out+='<ul class="menu files-list">';var arr1=it;if(arr1){var file,index=-1,l1=arr1.length-1;while(index<l1){file=arr1[index+=1];out+='<li class="item file" data-file-id="'+( file.id )+'"><a href="/files/'+( file.id )+'">'+( file.name )+'</a></li>';} } out+='</ul>';}else{out+='<div class="menu files-list"><p class="empty-page-list">You did not have upload any file now. Click on the link above to add your first one.</p></div>';}return out;
 };
   tmpl['files-show']=function anonymous(it) {
 var out='<img alt="'+( it.name ||'').toString().encodeHTML()+'" src="'+( it.content )+'" />';return out;
 };
   tmpl['list-main']=function anonymous(it) {
-var out='<h3> Pages </h3><a href="/pages/add" class="" id="add-page">Ajouter une page</a><ul class="menu pages-list">';if(it.length > 0){var arr1=it;if(arr1){var page,index=-1,l1=arr1.length-1;while(index<l1){page=arr1[index+=1];out+='<li class="item" data-page-id="'+( page.id )+'"><a href="/pages/'+( page.id )+'">'+( page.title )+'</a></li>';} } }else{out+='<p class="empty-page-list">Aucune page pour l\'instant. Pour créer votre première page, utiliser le lien ci-dessus.</p>';}out+='</ul>';return out;
+var out='<h3> Pages </h3><a href="/pages/add" class="" id="add-page">Add a page</a><ul class="menu pages-list">';if(it.length > 0){var arr1=it;if(arr1){var page,index=-1,l1=arr1.length-1;while(index<l1){page=arr1[index+=1];out+='<li class="item" data-page-id="'+( page.id )+'"><a href="/pages/'+( page.id )+'">'+( page.title )+'</a></li>';} } }else{out+='<p class="empty-page-list">There\'s no page for now. Click on the link above to create your first one.</p>';}out+='</ul>';return out;
 };
   tmpl['show-main']=function anonymous(it) {
 var out='<div class="show-wrapper"><div class="toolbar"></div><div class="content"></div></div>';return out;
@@ -727,7 +727,7 @@ var out='<div class="show-wrapper"><div class="toolbar"></div><div class="conten
 var out=''+( it );return out;
 };
   tmpl['show-toolbar']=function anonymous(it) {
-var out='<a class="icon icon-edit" id="edit-page" href="/pages/'+( it.id )+'/edit">Editer</a><div class="title">'+( it.title )+'</div>';return out;
+var out='<a alt="Edit" class="icon icon-edit" id="edit-page" href="/pages/'+( it.id )+'/edit">Edit</a><div class="title">'+( it.title )+'</div>';return out;
 };
 return tmpl;})();
 },{}],7:[function(require,module,exports){
@@ -4599,7 +4599,7 @@ EditorView.prototype.getValue = function() {
 
 module.exports = EditorView;
 
-},{"../tpl/templates.js":6,"./markedRenderer":21,"brace/mode/markdown":22,"brace/theme/chrome":20,"marked":16,"brace":23,"format":24,"page":4}],24:[function(require,module,exports){
+},{"../tpl/templates.js":6,"./markedRenderer":21,"brace/mode/markdown":22,"brace/theme/chrome":20,"brace":23,"marked":16,"format":24,"page":4}],24:[function(require,module,exports){
 (function(){//
 // format - printf-like string formatting for JavaScript
 // github.com/samsonjs/format
@@ -24450,38 +24450,7 @@ dom.importCssString(exports.cssText, exports.cssClass);
         
 module.exports = window.ace.acequire("ace/ace");
 })()
-},{"w3c-blob":28}],28:[function(require,module,exports){
-(function(global){module.exports = get_blob()
-
-function get_blob() {
-  if(global.Blob) {
-    try {
-      new Blob(['asdf'], {type: 'text/plain'})
-      return Blob
-    } catch(err) {}
-  }
-
-  var Builder = global.WebKitBlobBuilder ||
-                global.MozBlobBuilder ||
-                global.MSBlobBuilder
-
-  return function(parts, bag) {
-    var builder = new Builder
-      , endings = bag.endings
-      , type = bag.type
-
-    if(endings) for(var i = 0, len = parts.length; i < len; ++i) {
-      builder.append(parts[i], endings)
-    } else for(var i = 0, len = parts.length; i < len; ++i) {
-      builder.append(parts[i])
-    }
-
-    return type ? builder.getBlob(type) : builder.getBlob()
-  }
-}
-
-})(window)
-},{}],27:[function(require,module,exports){
+},{"w3c-blob":28}],27:[function(require,module,exports){
 (function(){/*global window:false, self:false, define:false, module:false */
 
 /**
@@ -25702,5 +25671,36 @@ function get_blob() {
 }, this);
 
 })()
+},{}],28:[function(require,module,exports){
+(function(global){module.exports = get_blob()
+
+function get_blob() {
+  if(global.Blob) {
+    try {
+      new Blob(['asdf'], {type: 'text/plain'})
+      return Blob
+    } catch(err) {}
+  }
+
+  var Builder = global.WebKitBlobBuilder ||
+                global.MozBlobBuilder ||
+                global.MSBlobBuilder
+
+  return function(parts, bag) {
+    var builder = new Builder
+      , endings = bag.endings
+      , type = bag.type
+
+    if(endings) for(var i = 0, len = parts.length; i < len; ++i) {
+      builder.append(parts[i], endings)
+    } else for(var i = 0, len = parts.length; i < len; ++i) {
+      builder.append(parts[i])
+    }
+
+    return type ? builder.getBlob(type) : builder.getBlob()
+  }
+}
+
+})(window)
 },{}]},{},[1])
 ;
